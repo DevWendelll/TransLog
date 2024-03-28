25517cfe9a491f97a9a128682f7de4d232f1077d
Senai <Senai@CEPEAS21104170>
2024-03-27 21:49:27 -0300

    feat - commit final

---
+++ TransLog/BackEnd/Models/Enums/BaseCalculos.cs
@@ -0,0 +1,9 @@
+﻿namespace BackEnd.Models.Enums
+{
+    public static class BaseCalculos
+    {
+        public static decimal ValorKm = 2;
+        public static decimal AdcionalKm = 1;
+        public static decimal AdcionalMetroCubico = 2;
+    }
+}
---
+++ TransLog/FrontEnd/Models/Request/InformacoesFreteRequest.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FrontEnd.Models.Request
+{
+    public class InformacoesFreteRequest
+    {
+        [Required(ErrorMessage = "Distancia é obrigatório")]
+        public int Distancia { get; set; }
+
+        [Required(ErrorMessage = "PesoCarga é obrigatório")]
+        public int PesoCarga { get; set; }
+
+        [Required(ErrorMessage = "Largura é obrigatório")]
+        public int Largura { get; set; }
+
+        [Required(ErrorMessage = "Altura é obrigatório")]
+        public int Altura { get; set; }
+
+        [Required(ErrorMessage = "Cumprimento é obrigatório")]
+        public int Cumprimento { get; set; }
+    }
+}
